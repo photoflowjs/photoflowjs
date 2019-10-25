df5e19f7ff59b8858816e4c0fd11d8d9af8c7014
Bryan Ehrlich <ehrlich.bryan@gmail.com>
2019-10-25 18:29:27 -0400

    Basic DAG layout engine

--- pictureflow.js
+++ pictureflow.js
@@ -1,3 +1,5 @@
+
+
 function pictureflowInstance(window, container, images) {
     this.container = container;
     this.images = images;
@@ -9,6 +11,12 @@ function pictureflowInstance(window, container, images) {
     this.border = 5;
     // TODO: Replace this with something fancy
     this.imagesPerRow = 3;
+    // TODO: User defined
+    this.rowHeightTarget = 600;
+    // TODO: User defined
+    this.minRowHeight = this.rowHeightTarget / 4;
+    // TODO: User defined
+    this.maxRowHeight = this.rowHeightTarget * 3;
     var _t = this;
     this.window.onresize = function () {
         _t._windowResized();
@@ -58,7 +66,7 @@ function pictureflowInstance(window, container, images) {
             var currentImage = images[i];
             // r = x / y
             // x = r * y
-            var width = (currentImage.width / currentImage.height) * rowHeight;
+            var width = (currentImage.naturalWidth / currentImage.naturalHeight) * rowHeight;
             window.pictureflow._positionImage(currentImage, currentX, startY, width, rowHeight);
             window.pictureflow._revealImage(currentImage);
             currentX += width + this.border;
@@ -66,8 +74,120 @@ function pictureflowInstance(window, container, images) {
 
         return rowHeight;
     }
+    this._genRowKey = function(ixArray) {
+        return ixArray.join(",");
+    }
+    this._rowHeight = function(ixArray) {
+        var imageRow = [];
+        for (var i = 0; i < ixArray.length; i++) {
+            imageRow.push(this.images[ixArray[i]]);
+        }
+        return this._calculateY(imageRow);
+    }
+    this._calculateBadness = function(height) {
+        return Math.pow(Math.abs(height - this.rowHeightTarget), 2);
+    }
+    this._isRowValid = function(ixArray) {
+        var y = this._rowHeight(ixArray);
+
+        return y >= this.minRowHeight && y <= this.maxRowHeight;
+    }
+    this._findOptimalRows = function() {
+        var totalImages = this.images.length;
+        var rowMap = {"root": {key:"root", height: -1, cost: 0, terminal: false, bestParent: null, children: []}};
+        var breakIx = [{ parent: "root", ix: 0 }];
+        var poppedIx;
+        while (popped = breakIx.pop()) {
+            var poppedIx = popped.ix;
+            var parent = popped.parent;
+            var transitionedToInvalid = false;
+            var lastValid = false;
+            var currentIx = poppedIx;
+            var currentRow = [poppedIx];
+            while (currentIx < totalImages && !transitionedToInvalid) {
+                if (this._isRowValid(currentRow)) {
+                    lastValid = true;
+                    var rowKey = this._genRowKey(currentRow);
+                    breakIx.push({ parent: rowKey, ix: currentIx + 1 });
+                    rowMap[parent].children.push(rowKey);
+                    // Make sure we didn't already calculate this
+                    if (!(rowKey in rowMap)) {
+                        var currentHeight = this._rowHeight(currentRow);
+                        rowMap[rowKey] = {
+                            key: rowKey,
+                            height: currentHeight,
+                            badness: this._calculateBadness(currentHeight),
+                            cost: Infinity,
+                            terminal: currentRow[currentRow.length - 1] === totalImages - 1,
+                            bestParent: null,
+                            children: []
+                        };
+                    }
+                } else {
+                    if (lastValid) {
+                        transitionedToInvalid = true;
+                    }
+                }
+                currentIx++;
+                currentRow.push(currentIx);
+            }
+        }
+
+        var bfs = [rowMap.root];
+        var node;
+        while (node = bfs.pop()) {
+            for (var i = 0; i < node.children.length; i++) {
+                var currentChild = rowMap[node.children[i]];
+                bfs.push(currentChild);
+                if (node.cost + currentChild.badness < currentChild.cost) {
+                    currentChild.cost = node.cost + currentChild.badness;
+                    currentChild.bestParent = node.key;
+                }
+            }
+        }
+
+        var rows = Object.keys(rowMap);
+        var bestPathEnd = null;
+        var bestPathCost = Infinity;
+
+        for (var i = 0; i < rows.length; i++) {
+            var currentRow = rowMap[rows[i]];
+            if (currentRow.terminal) {
+                if (currentRow.cost < bestPathCost) {
+                    bestPathCost = currentRow.cost;
+                    bestPathEnd = currentRow.key;
+                }
+            }
+        }
+
+        var bestPath = [];
+        var current = rowMap[bestPathEnd];
+        while (current) {
+            if (current.key !== "root") {
+                bestPath.push(current.key);
+            }
+            current = rowMap[current.bestParent];
+        }
+
+        return bestPath.reverse();
+    }
+    this._explodeRows = function(optimalRowKeys) {
+        var toReturn = [];
+        for (var i = 0; i < optimalRowKeys.length; i++) {
+            var imageChunk = [];
+            var currentRow = optimalRowKeys[i].split(",");
+            for (var j = 0; j < currentRow.length; j++) {
+                imageChunk.push(this.images[parseInt(currentRow[j])]);
+            }
+            toReturn.push(imageChunk);
+        }
+        return toReturn;
+    }
     this._doFlow = function() {
-        var chunks = chunk(this.images, this.imagesPerRow);
+        var rows = this._findOptimalRows();
+        console.log(rows);
+        var chunks = this._explodeRows(rows);
+        //var chunks = chunk(this.images, this.imagesPerRow);
         var y = 0;
         for (var i = 0; i < chunks.length; i++) {
             var currentY = this._emitRow(chunks[i], y);
@@ -78,7 +198,7 @@ function pictureflowInstance(window, container, images) {
     this._getRs = function (images) {
         var r = [];
         for (var i = 0; i < images.length; i++) {
-            r.push(images[i].width / images[i].height);
+            r.push(images[i].naturalWidth / images[i].naturalHeight);
         }
         return r;
     }
